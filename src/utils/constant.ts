4026826cdfdad56e426c4799c4ddf6a9fa7aeead
android <hoangquan225.qh@gmail.com>
2023-06-20 14:05:19 +0700

    update .env

---
+++ src/utils/constant.ts
@@ -0,0 +1,3 @@
+export const TOKEN_EXPIRED = 24 * 60 * 60;
+export const COOKIE_EXPIRED = 30;
+export const TOKEN_FORGOT_PASS_EXPIRED = 10 * 60;
--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -1,12 +1,11 @@
 import { CookieOptions } from 'express';
 import dotenv from './dotenv';
+import { COOKIE_EXPIRED } from './constant';
 dotenv.config();
 
 // export const getCookieOptions = (maxAge: number = 31536000000) => {
 export const getCookieOptions = (
-  expires = new Date(
-    Date.now() + Number(process.env.COOKIE_EXPIRED) * 24 * 60 * 60 * 1000
-  ),
+  expires = new Date(Date.now() + COOKIE_EXPIRED * 24 * 60 * 60 * 1000),
   maxAge?: any
 ) => {
   const cookieOptions: CookieOptions = {
