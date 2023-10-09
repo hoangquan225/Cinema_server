e4a752006aa3304ff8ed6bd6e6e141b8a3b9f0e9
quanhh <quanhh@vnpt.vn>
2023-10-09 17:05:52 +0700

    update

--- src/api/mobile/film.ts
+++ src/api/mobile/film.ts
@@ -6,11 +6,13 @@ import { Film } from '../../models/film';
 const router = Router();
 const filmServices = new FilmServices();
 
-router.post("/get-film-by-id", asyncHandler(async (req, res) => {
+router.get("/get-film-by-id", asyncHandler(async (req, res) => {
     if (!req.query.filmId) return res.json({
         data: 'cannot find param categoryid',
         status: -1
     })
+    console.log(req.query.filmId);
+    
     const id = `${req.query.filmId}`
     const data = await filmServices.getFilmById(id)
     return res.json(data)
